f071543c715680d6887ac15b2c4832e79ce35ea1
takihama <danielcalderonuns@gmail.com>
2022-02-13 18:42:48 -0300

    Tutorial part1: Buttons, Event Handlers, Destructuring

--- tutorials/part1/src/App.js
+++ tutorials/part1/src/App.js
@@ -1,20 +1,45 @@
-const Hello = (props) => {
+import { useState } from 'react'
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>)
+  }
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old.</p>
+      button press history: {props.allClicks.join(' ')}
+    </div>)
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
 
-const App = () => {
-  const name = "Daniel"
-  const age = 26
-  return (
-    <>
-     <h1>Greetings</h1>
-     <Hello name={name} age={age}/>
-    </>
-  )
-}
-
-export default App;
+export default App
