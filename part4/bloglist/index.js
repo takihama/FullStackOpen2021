52bc3072cd58524fc40f35a287c98a8f94f67d99
takihama <danielcalderonuns@gmail.com>
2022-04-07 20:55:11 -0300

    4.2 - Refactor index.js and app.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,29 +1,10 @@
+const app = require('./app') // the actual Express application
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const { MONGODB_URL, PORT } = require('./utils/config')
-const { info, error } = require('./utils/logger')
-const Blog = require('./models/blog')
-const middleware = require('./utils/middleware')
-const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-mongoose.connect(MONGODB_URL)
-  .then(() => {
-    info('Connected to MongoDB')
-  })
-  .catch(err => {
-    error('Error connecting to MongoDB:', err.message)
-  })
+const server = http.createServer(app)
 
-app.use(cors()) // Allow requests from multiple origins
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-app.listen(PORT, () => {
-  info(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
