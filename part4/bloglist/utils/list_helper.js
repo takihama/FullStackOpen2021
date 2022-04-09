6461a42b917e9e65f1c97f8cbf1f7f24e0a12a8c
takihama <danielcalderonuns@gmail.com>
2022-04-09 12:11:30 -0300

    4.4 - Test total likes

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,9 @@
-const dummy = (blogs) => {
-  return 1
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, blog) => {
+    return acc + blog.likes
+  }, 0)
 }
 
 module.exports = {
-  dummy
+  totalLikes
 }
